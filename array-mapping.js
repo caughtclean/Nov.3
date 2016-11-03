3632519fb73d99a23fdc14f8802839360374cdba
Nathan Eyerley <eyerleyn@gmail.com>
2016-11-03 21:12:05 +0000

    added array mapping challange

---
+++ array-mapping.js
@@ -0,0 +1,19 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+
+var result = input.map(function(values) {
+  var xySquared = Math.pow(values.x, 2) + Math.pow(values.y, 2);
+  var result = Math.sqrt(xySquared);
+  return result;
+
+
+});
+
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
